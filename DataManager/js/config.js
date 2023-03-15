db10a3a8b3e5ede5ac50ccdaacfd65927ee869fc
JsJunz <101870254+JsJunz@users.noreply.github.com>
2023-03-15 23:38:25 +0100

    Update config.js

--- DataManager/js/config.js
+++ DataManager/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolClientId: '4sq3675cmv9mdtf7m4jnil8rfq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1', // e.g. us-east-2
-        authDomainPrefix: 'poc1-saml-federation', // e.g. leosaml
-        redirect_uri:'https://dia3bgk3gajed.cloudfront.net' //e.g. https://d*******.cloudfront.net
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '', // e.g. us-east-2
+        authDomainPrefix: '', // e.g. leosaml
+        redirect_uri:'' //e.g. https://d*******.cloudfront.net
     },
     api: {
-        invokeUrl: 'https://u0b7qp7q62.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://xxxxxxxx.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://xxxxxxxx.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
